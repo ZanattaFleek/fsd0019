4411ef0010841ba13287b33483387c5d87c45342
ZanattaFleek <zanattafleek@gmail.com>
2023-02-16 08:33:55 -0300

    ReunificacaoBranchs

--- ProjetoBaseReact/src/testes_apagar/Aula.tsx
+++ ProjetoBaseReact/src/testes_apagar/Aula.tsx
@@ -1,9 +1,19 @@
+/*
+
+xs, extra-small: 0px
+sm, small: 600px
+md, medium: 900px
+lg, large: 1200px
+xl, extra-large: 1536px
+
+*/
+
 import React from 'react'
 
 //import Box from '@mui/material/Box';
 //import Button from '@mui/material/Button';
 
-import { Box, Button } from '@mui/material'
+import { Box, Button, createTheme, Select, TextField, ThemeProvider } from '@mui/material'
 import Container from '@mui/material/Container'
 import Grid from '@mui/material/Grid'
 import { styled } from '@mui/material/styles'
@@ -21,25 +31,62 @@ const Item = styled(Paper)(({ theme }) => ({
 
 export default function Aula() {
 
+  const tema = createTheme({
+    palette: {
+      primary: {
+        main: '#ff00aa',
+      },
+      secondary: {
+        main: '#aa00ff',
+      },
+    },
+
+  });
+
   return (
     <>
-      <Box>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
+      <ThemeProvider theme={tema}>
+        <Paper sx={{
+          p: 2,
+          margin: 'auto',
+          maxWidth: 600,
+          flexGrow: 1,
+          backgroundColor: (theme) =>
+            theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+        }}>
+          <Grid
+            container
+            spacing={1}
+          >
+            <Grid item xs={12}>
+              <TextField size='small' sx={{ width: '100%' }} label="Nome" />
+            </Grid>
+            <Grid item xs={12} md={3}>
+              <TextField size='small' sx={{ width: '100%' }} label="Cep" />
+            </Grid>
+            <Grid item xs={12} md={9}>
+              <TextField size='small' sx={{ width: '100%' }} label="Endereço Completo" />
+            </Grid>
+            <Grid item xs={12} md={5}>
+              <TextField size='small' sx={{ width: '100%' }} label="Bairro" />
+            </Grid>
+            <Grid item xs={10} md={5}>
+              <TextField size='small' sx={{ width: '100%' }} label="Cidade" />
+            </Grid>
+            <Grid item xs={2}>
+              <TextField size='small' sx={{ width: '100%' }} label="UF" />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField size='small' sx={{ width: '100%' }} label="Telefone" />
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <TextField size='small' sx={{ width: '100%' }} label="Data Nascimento" />
+            </Grid>
+
           </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={4}>
-            <Item>xs=4</Item>
-          </Grid>
-          <Grid item xs={8}>
-            <Item>xs=8</Item>
-          </Grid>
-          
-        </Grid>
-      </Box>
+
+        </Paper>
+      </ThemeProvider>
     </>
   )
 }
